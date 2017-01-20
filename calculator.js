4531ada95966ab9ecfca20c50271904e181639fb
Philip White <phil_23@live.co.uk>
2017-01-20 12:21:37 +0000

    added base files, layout in html, and basic maths functions

---
+++ calculator.js
@@ -0,0 +1,18 @@
+
+
+var add = function(number1, number2)
+{
+  return number1 + number2;
+}
+var minus = function(number1, number2)
+{
+  return number1 - number2;
+}
+var divide = function(number1,number2)
+{
+  return number1/number2;
+}
+var multiply = function(number1, number2)
+{
+  return  number1 * number2;
+}
